ebd986db9023f3a0bac0c3d9eba217db41c0d30d
haizeiwang <mlsumin@163.com>
2019-03-23 17:31:53 +0800

    base_app动作库

---
+++ base/base.py
@@ -0,0 +1,25 @@
+class base():
+    def __init__(self, driver):
+        self.driver = driver
+
+    # 定位元素
+    def base_find_element(self, loc):
+        return self.driver.find_element(*loc)
+
+    # 定位一组
+    def base_find_elements(self, loc):
+        return self.driver.find_elements(*loc)
+
+    # 点击定位一个
+    def base_click(self, ele):
+        self.base_find_element(ele).click()
+
+    # 点击定位一组
+    def base_clicks(self, ele, num):
+        self.base_find_elements(ele)[num].click()
+
+    # 输入
+    def base_input(self, ele, values):
+        el = self.base_find_element(ele)
+        el.clear()
+        el.send_keys(values)
